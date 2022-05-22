13d94b8de7629ed0d09542dca22145dc11213452
veeresh soni <veereshsoni913@gmail.com>
2022-05-22 20:20:38 +0530

    00

--- attendenceProject.py
+++ attendenceProject.py
@@ -76,10 +76,11 @@ while True:
     #showing the same image as captured via webcam
     cv2.imshow('Webcam', img)
     cv2.waitKey(1)
-            # y1,x2,y2,x1 = faceLoc
-            # y1, x2, y2, x1 = y1*4,x2*4,y2*4,x1*4
-            # cv2.rectangle(img, (x1,y1), (x2,y2), (0,255,0), 2)
-            # cv2.rectangle(img, (x1,y2-35), (x2,y2), (0,255,0), cv2.FILLED)
-            # cv2.putText(img, name, (x1+6,y2-6), cv2.FONT_HERSHEY_COMPLEX, 1, (255,255,255), 2)
 
+            
 
+# y1,x2,y2,x1 = faceLoc
+# y1, x2, y2, x1 = y1*4,x2*4,y2*4,x1*4
+# cv2.rectangle(img, (x1,y1), (x2,y2), (0,255,0), 2)
+# cv2.rectangle(img, (x1,y2-35), (x2,y2), (0,255,0), cv2.FILLED)
+# cv2.putText(img, name, (x1+6,y2-6), cv2.FONT_HERSHEY_COMPLEX, 1, (255,255,255), 2)
